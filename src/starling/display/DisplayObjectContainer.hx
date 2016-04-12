add324cf3211069ff913ab3651df010dfe7cb985
Tom Byrne <tom.byrne@imagination.com>
2016-04-12 11:02:08 +1000

    Fixed issue where last child wasn't removed when removeChildren() was called

--- src/starling/display/DisplayObjectContainer.hx
+++ src/starling/display/DisplayObjectContainer.hx
@@ -204,8 +204,8 @@ class DisplayObjectContainer extends DisplayObject
 	 *  If no arguments are given, all children will be removed. */
 	public function removeChildren(beginIndex:Int=0, endIndex:Int=-1, dispose:Bool=false):Void
 	{
-		if (endIndex < 0 || endIndex >= numChildren) 
-			endIndex = numChildren - 1;
+		if (endIndex < 0 || endIndex > numChildren) 
+			endIndex = numChildren;
 		
 		for (i in beginIndex...endIndex)
 			removeChildAt(beginIndex, dispose);
